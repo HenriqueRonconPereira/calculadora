864de6a61b3ccd52c0335af17e912dc338716dbe
HenriqueRonconPereira <henriqueroncon@hotmail.com>
2024-02-15 18:18:12 -0300

    criação do modo escuro e modo claro já funcional

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const [darkMode, setDarkMode] = useState(false)
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +15,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  results: {
+    backgroundColor: darkMode ? "#282f3b" : "#f5f5f5",
+    width: '328%',
+    minHeight: 280,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+
+  },
+  resultText: {
+    margin: 10,
+    fontSize: 25,
+    color: darkMode ? "#fff" : "#000",
+  },
+  themeButton: {
+    alignSelf: 'flex-start',
+    bottom: 130,
+    margin: 15,
+    backgroundColor: darkMode ? "#7b8084" : "#e5e5e5",
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 45,
+    height: 45,
+    borderRadius: 25, 
+  }
 });
+
+  return (
+    <View>
+      <View style={styles.results}>
+        <TouchableOpacity style={styles.themeButton}>
+          <Entypo name={darkMode ? "light-up" : 'moon'} size={24} color={darkMode ? "white" : 'black'}
+           onPress={() => darkMode ? setDarkMode(false) : setDarkMode(true)}/> 
+        </TouchableOpacity>
+        <Text style={styles.resultText}>2 + 2 = 5</Text>
+      </View>
+      <View style={styles.buttons}>
+
+      </View>
+    </View>
+  );
+}
+
+
